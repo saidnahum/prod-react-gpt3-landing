9b08b185ee7fb13dc022ae1c13d13cbb163f10e9
saidnahum <saidnahum@gmail.com>
2021-11-12 18:00:25 -0600

    Finish setup

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cta.css';
+
+const Cta = () => {
+   return (
+      <div>
+         <h1>CTA Component</h1>
+      </div>
+   )
+}
+
+export default Cta
---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './blog.css';
+
+const Blog = () => {
+   return (
+      <div>
+         <h1>Blog Container</h1>
+      </div>
+   )
+}
+
+export default Blog;
