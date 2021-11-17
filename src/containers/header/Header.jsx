1ada1c166402160861eb89d758358999e9b48cae
saidnahum <saidnahum@gmail.com>
2021-11-17 17:55:49 -0600

    finished app

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
             <div className="gpt3__header-content__input">
-               <input type="email" placeholder='Your Email Address'/>
+               <input type="email" placeholder='Your Email Address' />
                <button type='button'>Get Started</button>
             </div>
 
@@ -20,10 +20,10 @@ const Header = () => {
                <img src={people} alt="people" />
                <p>1,600 people requested access a visit in last 24 hours</p>
             </div>
-
-            {/* <div className="gpt3__header-image">
-               <img src={ai} alt="ai" />
-            </div> */}
+         </div>
+         
+         <div className="gpt3__header-image">
+            <img src={ai} alt="ai" />
          </div>
       </div>
    )
